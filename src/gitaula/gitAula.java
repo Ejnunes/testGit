4ca2cb6ebafbfe7d04fab4a1544978f73c1dc623
Edsonjn <edsonjnunes@hotmail.com>
2023-08-17 16:42:12 -0300

    Meu primeiro commit

---
+++ src/gitaula/gitAula.java
@@ -0,0 +1,10 @@
+package gitaula;
+
+public class gitAula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author edson.nunes1
+ *
+ */
+module gitaula {
+}
